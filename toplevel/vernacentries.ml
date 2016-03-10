be94c2116043323ffebfba98163b52daf7146f5a
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2016-03-10 09:45:29 +0100

    -

--- toplevel/vernacentries.ml
+++ toplevel/vernacentries.ml
@@ -1886,7 +1886,11 @@ let _ = msg_notice (Printmod.pr_mutual_inductive_body env sp (Environ.lookup_min
           let dir = (path,[]) in
           let interp (loc : Loc.t) (bi : Bigint.bigint) : Glob_term.glob_constr =
 let _ = Printf.eprintf "*** big int %s\n%!" (Bigint.to_string bi) in
-let _ : unit = vernac_check_may_eval None None (CCast(loc,f,CastConv g)) in
+            let bt =
+              ([(loc, Anonymous)], Default Implicit,
+               CRef (Ident (loc, Id.of_string "bigint"), None)) in
+            let c = CProdN (loc, [bt], CRef (Qualid (loc, qid), None)) in
+            let _ = vernac_check_may_eval None None (CCast(loc, f, CastConv c)) in
             failwith "Number Notation (interp) not yet interpreted"
           in
           let patl : Glob_term.glob_constr list = [] in
