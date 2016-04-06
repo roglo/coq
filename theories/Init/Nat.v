97ab0ab5140b69372db5b65adffdf55bc3167bb4
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2016-04-06 10:57:24 +0200

    -

--- theories/Init/Nat.v
+++ theories/Init/Nat.v
@@ -20,25 +20,19 @@ Local Open Scope nat_scope.
 
 Definition t := nat.
 
-(** ** Constants *)
-
-Definition zero := 0.
-Definition one := 1.
-Definition two := 2.
-
 (** ** Basic operations *)
 
 Definition succ := S.
 
 Definition pred n :=
   match n with
-    | 0 => n
+    | O => n
     | S u => u
   end.
 
 Fixpoint add n m :=
   match n with
-  | 0 => m
+  | O => m
   | S p => S (p + m)
   end
 
@@ -48,11 +42,80 @@ Definition double n := n + n.
 
 Fixpoint mul n m :=
   match n with
-  | 0 => 0
+  | O => O
   | S p => m + p * m
   end
 
 where "n * m" := (mul n m) : nat_scope.
+
+(** ** Parsing and Printing digit strings as type nat *)
+
+Inductive positive' : Set :=
+  | x'I : positive' -> positive'
+  | x'O : positive' -> positive'
+  | x'H : positive'.
+
+Inductive Z' : Set :=
+  | Z'0 : Z'
+  | Z'pos : positive' -> Z'
+  | Z'neg : positive' -> Z'.
+
+Fixpoint pos'pred_double x :=
+  match x with
+  | x'I p => x'I (x'O p)
+  | x'O p => x'I (pos'pred_double p)
+  | x'H => x'H
+  end.
+
+Definition nat_of_Z' x :=
+  match x with
+  | Z'0 => Some O
+  | Z'pos p =>
+      let fix iter p a :=
+        match p with
+        | x'I p0 => a + iter p0 (a + a)
+        | x'O p0 => iter p0 (a + a)
+        | x'H => a
+        end
+      in
+      Some (iter p (S O))
+  | Z'neg p => None
+  end.
+
+Fixpoint pos'succ x := 
+  match x with
+  | x'I p => x'O (pos'succ p)
+  | x'O p => x'I p
+  | x'H => x'O x'H
+  end.
+
+Definition Z'succ x := 
+  match x with
+  | Z'0 => Z'pos x'H
+  | Z'pos x' => Z'pos (pos'succ x')
+  | Z'neg x' =>
+      match x' with
+      | x'I p => Z'neg (x'O p)
+      | x'O p => Z'neg (pos'pred_double p)
+      | x'H => Z'0
+      end
+  end.
+
+Fixpoint Z'_of_nat_loop n :=
+  match n with
+  | O => Z'0
+  | S p => Z'succ (Z'_of_nat_loop p)
+  end.
+
+Definition Z'_of_nat n := Some (Z'_of_nat_loop n).
+
+Number Notation nat nat_of_Z' Z'_of_nat : nat_scope (warning after 5000).
+
+(** ** Constants *)
+
+Definition zero := 0.
+Definition one := 1.
+Definition two := 2.
 
 (** Truncated subtraction: [n-m] is [0] if [n<=m] *)
 
